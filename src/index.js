aa2d858d4545e8e1b77a8ec6ffc0a21da6baea71
idaols <ida.olsson@metropolia.fi>
2022-01-10 11:39:41 +0200

    tunnilla tehtyjä esimerkkejä

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@
 console.log('Hello console!');
 console.log('Heippa!');
 
+// aina kun esitellään uusi muuttuja, käytä const/let
+// jos ei ole tarkoitus muuttaa arvoa, käytä const (vakio)
+const number = 3;
+
+const array = ['a', 'b'];
+array.push('c');
+console.log(array);
+
+// numeron tilalle voi tallettaa stringin, ei tule erroria, mutta ole tarkkana näissä
+// suosi kolmen merkin vertailuja, esim. === tai !==
+// huom! älä tee laskuoperaatioita merkkijonoilla
+// (1==2) ? 'yhtäsuuri' : 'erisuuri'; , kysymysmerkillä simppeli if-lause
+
+
+
+
+
