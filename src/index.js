e26ad794154f6d62799df3762931ae4c30a32dc6
idaols <ida.olsson@metropolia.fi>
2022-01-20 16:47:11 +0200

    aloitettu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-/**TASK 1 */
+const menu =
+  [
+    { name: 'Lingonberry jam', price: 4.00 },
+    { name: 'Mushroom and bean casserole', price: 5.50 },
+    { name: 'Chili-flavoured wheat', price: 3.00 },
+    { name: 'Vegetarian soup', price: 4.80 },
+    { name: 'Pureed root vegetable soup with smoked cheese', price: 8.00 }
+  ];
 
-console.log('Hello console!');
+
+const validator = () => {
+  const regex = /^[A-ZÖÄÅ]{1}[a-zöäå,A-ZÖÄÅ/0-9()-\s]{4,64}$/;
+
+};
+
